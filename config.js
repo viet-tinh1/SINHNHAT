04e70b7277d4b85f3b013a898899bf52c8d209fe
+Tinh Nguyễn _ <thunguyen2572004@gmail.com>
2024-11-22 19:58:54 +0700

    sinhnhat

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Nhấn vào đây nè';
+const giftUrl = 'https://youtu.be/Wu8NeFXaoOc?si=-RKbMb188oF6L4iC';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = `Chúc Quỳnh một sinh nhật thật vui vẻ và trọn vẹn! Mong rằng những điều tuyệt vời nhất sẽ đến với Quỳnh, từ những ước mơ nhỏ bé đến những dự định lớn lao.
+Với anh, Quỳnh luôn là một người đặc biệt và là nguồn cảm hứng mỗi ngày. Hãy giữ mãi nụ cười rạng rỡ trên môi vì nó khiến mọi thứ xung quanh thêm tươi đẹp.
+Chúc Quỳnh một tuổi mới đầy ắp niềm vui, hạnh phúc và thật nhiều kỷ niệm đáng nhớ! ❤️`;
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
