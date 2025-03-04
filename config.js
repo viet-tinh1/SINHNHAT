32bd59e626e7d74def4fea034b22a6ab88c799e9
+Tinh Nguyễn _ <tinhnguyen1582002@gmail.com>
2025-03-04 21:55:29 +0700

    add comit

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const nameGirl = 'Nhấn vào đây nè';
-const giftUrl = 'https://youtu.be/Wu8NeFXaoOc?si=-RKbMb188oF6L4iC';
-const eventName = 'Chúc Mừng Sinh Nhật';
+const giftUrl = 'https://www.youtube.com/watch?v=3qTCqaBV15k';
+const eventName = 'Chúc Mừng 8/3 ';
 const titleCard = 'Tặng người ấy';
-const contentCard = `Chúc Quỳnh một sinh nhật thật vui vẻ và trọn vẹn! Mong rằng những điều tuyệt vời nhất sẽ đến với Quỳnh, từ những ước mơ nhỏ bé đến những dự định lớn lao.
-Với anh, Quỳnh luôn là một người đặc biệt và là nguồn cảm hứng mỗi ngày. Hãy giữ mãi nụ cười rạng rỡ trên môi vì nó khiến mọi thứ xung quanh thêm tươi đẹp.
-Chúc Quỳnh một tuổi mới đầy ắp niềm vui, hạnh phúc và thật nhiều kỷ niệm đáng nhớ! ❤️`;
+const contentCard = `Hân à,
+
+Chúc em một ngày 8/3 thật vui vẻ và tràn đầy hạnh phúc! Với anh, em luôn là một người đặc biệt – không chỉ hôm nay mà mỗi ngày đều xứng đáng nhận những điều tốt đẹp nhất.
+
+Hãy luôn giữ nụ cười rạng rỡ ấy nhé, vì nó làm mọi thứ xung quanh thêm tươi đẹp. Hy vọng một ngày nào đó, anh cũng có thể trở thành một phần trong những niềm vui của em. 🌸✨ ❤️`;
 const giftImage = 'hot-girl.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
