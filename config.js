df913f6cf754cfa615375a5ce6e9dbbdc01aefa0
+Tinh Nguyễn _ <tinhnguyen1582002@gmail.com>
2025-03-06 15:41:20 +0700

    Quỳnh

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Nhấn vào đây nè';
 const giftUrl = 'https://www.youtube.com/watch?v=3qTCqaBV15k';
 const eventName = 'Chúc Mừng 8/3 ';
 const titleCard = 'Tặng người ấy';
-const contentCard = `Oanh iu dấu à,
+const contentCard = `Quỳnh à,
 
 Chúc em một ngày 8/3 thật vui vẻ và tràn đầy hạnh phúc! Với anh, em luôn là một người đặc biệt – không chỉ hôm nay mà mỗi ngày đều xứng đáng nhận những điều tốt đẹp nhất.
 
